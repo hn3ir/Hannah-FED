d61760f57711cc965932af8ba00d4c8caf662724
Hannah Neirman <38301278+hn3ir@users.noreply.github.com>
2020-03-05 06:34:03 -0600

    Create practice.js

---
+++ practice.js
@@ -0,0 +1,86 @@
+/***************************************************************************/
+
+//Given this nested object
+var library = [
+    {
+        title: "Our Revolution",
+        author: "Bernie Sanders",
+        finishedReading: false
+    },
+    {
+        title: "The Catcher In The Rye",
+        author: "J.D. Salinger",
+        finishedReading: true
+    },
+    {
+        title: "Animal Farm",
+        author: "George Orwell",
+        finishedReading: true
+    },
+    {
+        title: "Anthem",
+        author: "Ayn Rand",
+        finishedReading: true
+    },
+    {
+        title: "The Hobbit",
+        author: "J.R.R. Tolkien",
+        finishedReading: false
+    }
+];
+
+/* Display the reading status of all books in the library object.  For example:
+    For books you have finished reading, display  
+        You have finished reading "Animal Farm" by George Orwell.
+    And
+        You have not finished reading "Our Revolution" by Bernie Sanders.*/
+
+
+
+
+
+/* Write a function that checks if the passed string is a palindrome or not.  A Palindrome is a word, phrase, or sequence that reads the same way forward and backwards. For example, the 'madam', 'nun', or 'dad' */
+
+function checkPalindrome(palindrome) {
+   // if statement
+};
+
+console.log(checkPalindrome("hannah"));
+
+
+
+
+
+
+// Combine the elements in the following Array Object to a single string
+var words = ["I", "like", "to", "play", "in", "the", "dirt"];
+
+
+
+
+
+/* Write a function that reverses any sequence of numbers and consoles the results. For Example: 
+    Your function should reverse 1836654 to 4566381 */
+//your code goes here
+
+
+
+
+
+// Write a program that calculates the average grade of the class in percentage and letter-grade given the following students and scores:  (Assume 100-90% is A, 89-80% is B, and so forth........). Print the results to the console in the following format:
+
+/*
+Average Grade: 84.43
+Letter Grade : B
+*/
+
+// change values to check results
+var scores = [
+   ['Walter', 100],
+   ['Jessie', 67],
+   ['Tuco', 64],
+   ['Gus', 98],
+   ['Saul', 88],
+   ['Mike', 92],
+   ['Hank', 82]
+];
