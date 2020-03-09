a58a4f2bd0f578812ad6fe5d63c9ba414a3f4451
Hannah Neirman <38301278+hn3ir@users.noreply.github.com>
2020-03-08 20:51:38 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,77 @@
+// 1. Define UI variables
+// Add task button as an event. Passes when the user sets add task button
+//
+//
+//
+//
+
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+// load all event listeners in one function
+loadEventListener();
+
+function loadEventListener(){
+    form.addEventListener('submit', addTask);
+
+    // remove task event
+    taskList.addEventListener('click', removeTask);
+    // click event becaue you are clicking that button
+
+    // clear tasks event
+    clearBtn.addEventListener('click', clearTask);
+
+    // filter task
+    taskInput.addEventListener('keyup', filterTask);
+
+}
+
+// ^ create functions for all of these
+// add task
+
+function addTask(e){
+    // validate  the input is not empty when user click 'add task'
+    if(taskInput.value === ''){
+        alert('Hey! Add a task dummy!');
+        return;
+    }
+
+    // create elements
+        // create li element (li)
+        var li = document.createElement('li');
+        li.className = 'collection-item';
+        li.appendChild(document.createTextNode(taskInput.value));
+
+        // link (a tag)
+        var link = document.createElement('a');
+        link.className='delete-item secondary-content';
+        link.innerHTML = '<i class="fa fa-remove"></i>';
+        li.appendChild(link);
+
+        taskList.appendChild(li);
+
+        // clear input
+        taskInput.value = '';
+
+    console.log(li);
+    console.log(link);
+    e.preventDefault();
+}
+
+// remove tasks
+function removeTask(e){
+
+}
+
+// clear asks
+function clearTask(e){
+
+}
+
+// filter tasks
+function filterTask(e){
+
+}
