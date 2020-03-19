e74b23f18c288a07c70d927598a1ee281cd6ad0d
Hannah Neirman <38301278+hn3ir@users.noreply.github.com>
2020-03-18 20:34:14 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -5,6 +5,8 @@
 //
 //
 
+alert(123);
+
 const form = document.querySelector('#task-form');
 const taskList = document.querySelector('.collection');
 const clearBtn = document.querySelector('.clear-tasks');
