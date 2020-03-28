bada89336faf2000233ac11f37154f4d22b764aa
hn3ir <hneirman@gmail.com>
2020-03-28 14:08:40 -0500

    javascript drum kit

--- JavaScript-task-list/app.js
+++ JavaScript-task-list/app.js
@@ -5,42 +5,42 @@
 //
 //
 
-<<<<<<< HEAD
-=======
-alert(123);
->>>>>>> 1d0a12c65867d893df252630c821cd5dac1fb749
 
 const form = document.querySelector('#task-form');
 const taskList = document.querySelector('.collection');
 const clearBtn = document.querySelector('.clear-tasks');
-    const filter = document.querySelector('#filter');
-    const taskInput = document.querySelector('#task');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
 
 
 
 // load all event listeners in one function
 loadEventListener();
 
-function loadEventListener(){
-    form.addEventListener('submit', addTask);
-<<<<<<< HEAD
-    
-=======
-    addTaskButton.addEventListener("click", () => {
-	    let taskItem = document.createElement("li");
-	    taskItem.textContent = addTaskInput.value;
-	    taskList.appendChild(taskItem);
-	    addTaskInput.value = " ";
-	});
+function loadEventListener() {
+    form.addEventListener('submit', addTask); <<
+    <<
+    << < HEAD
 
-	document.onclick = function(e)
-	{
-	  if(e.target.tagName == 'LI'){
-	     e.target.remove();
-	   }
-	}
+        ===
+        ===
+        =
+        addTaskButton.addEventListener("click", () => {
+            let taskItem = document.createElement("li");
+            taskItem.textContent = addTaskInput.value;
+            taskList.appendChild(taskItem);
+            addTaskInput.value = " ";
+        });
 
->>>>>>> 1d0a12c65867d893df252630c821cd5dac1fb749
+    document.onclick = function (e) {
+            if (e.target.tagName == 'LI') {
+                e.target.remove();
+            }
+        }
+
+        >>>
+        >>>
+        > 1 d0a12c65867d893df252630c821cd5dac1fb749
     // remove task event
     taskList.addEventListener('click', removeTask);
     // click event becaue you are clicking that button
@@ -56,32 +56,32 @@ function loadEventListener(){
 // ^ create functions for all of these
 // add task
 
-function addTask(e){
+function addTask(e) {
     // validate  the input is not empty when user click 'add task'
-    if(taskInput.value === ''){
+    if (taskInput.value === '') {
         alert('Hey! Add a task dummy!');
         return;
     }
 
     // create elements
-        // create li element (li)
-        var li = document.createElement('li');
-        li.className = 'collection-item';
-        li.appendChild(document.createTextNode(taskInput.value));
+    // create li element (li)
+    var li = document.createElement('li');
+    li.className = 'collection-item';
+    li.appendChild(document.createTextNode(taskInput.value));
 
-        // link (a tag)
-        var link = document.createElement('a');
-        link.className='delete-item secondary-content';
-        link.innerHTML = '<i class="fa fa-remove"></i>';
-        li.appendChild(link);
+    // link (a tag)
+    var link = document.createElement('a');
+    link.className = 'delete-item secondary-content';
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    li.appendChild(link);
 
-        taskList.appendChild(li);
+    taskList.appendChild(li);
 
-        // clear input
-        taskInput.value = '';
+    // clear input
+    taskInput.value = '';
 
 
-        
+
     console.log(li);
     console.log(link);
     e.preventDefault();
@@ -89,30 +89,22 @@ function addTask(e){
 
 // remove tasks
 
-function removeTask(e){
+function removeTask(e) {
     var ulElem = document.getElementById('.collection-item');
 
-<<<<<<< HEAD
-      }
-=======
-}
-removeTask.addEventListener('click', function(e) {
-        node.parentNode.removeChild('li') ;
+
+    removeTask.addEventListener('click', function (e) {
+        node.parentNode.removeChild('li');
     });
->>>>>>> 1d0a12c65867d893df252630c821cd5dac1fb749
 
 
-// clear tasks
-function clearTask(e){
-    var parent = this.parentElement.parentElement;
-    parent.removeChild(this.parentElement);
-}
+    // clear tasks
+    function clearTask(e) {
+        var parent = this.parentElement.parentElement;
+        parent.removeChild(this.parentElement);
+    }
 
-// filter tasks
-function filterTask(e){
+    // filter tasks
+    function filterTask(e) {
 
-}
-<<<<<<< HEAD
-
-=======
->>>>>>> 1d0a12c65867d893df252630c821cd5dac1fb749
+    }
